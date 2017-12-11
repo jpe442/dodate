caff01d76db467fa2c0c32db9444be32bde86c38
Josh <shamanseye@gmail.com>
2017-12-11 11:29:37 -0800

    Generate and complete Users controller.

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,32 @@
+class Api::UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 401
+    end
+  end
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'users/create'
+  end
+
+  namespace :api do
+    get 'users/index'
+  end
+
+  namespace :api do
+    get 'users/destroy'
+  end
+
+  namespace :api do
+    get 'users/update'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
