bbd772c00a867337da5a24b45799c8021bd74853
Josh <shamanseye@gmail.com>
2017-12-28 16:47:52 -0800

    Adjust CORS.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :logged_in?
 
-  private
+  # private
 
   def current_user
     return nil unless session[:session_token]
--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Dodate
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :patch, :delete]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :patch, :delete, :put]
       end
     end
 
